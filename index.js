65282f46a9c166318717d08856ec84fac6b337f8
Jon Stokes <jon.m.stokes@gmail.com>
2017-11-13 18:15:49 -0600

    Handle user confirmation

--- index.js
+++ index.js
@@ -7,69 +7,12 @@ var AWSCognito = require("amazon-cognito-identity-js");
 var apigClientFactory = require("aws-api-gateway-client").default;
 var WindowMock = require("window-mock").default;
 
+var argv = require("./secrets.json");
+
 global.window = { localStorage: new WindowMock().localStorage };
 global.navigator = function() {
   return null;
 };
-
-var argv = require("yargs")
-  .option("username", {
-    describe: "Username of the user",
-    demandOption: true
-  })
-  .option("password", {
-    describe: "Password of the user",
-    demandOption: true
-  })
-  .option("user-pool-id", {
-    describe: "Cognito user pool id",
-    demandOption: true
-  })
-  .option("app-client-id", {
-    describe: "Cognito user pool app client id",
-    demandOption: true
-  })
-  .option("cognito-region", {
-    describe: "Cognito region",
-    default: "us-east-1"
-  })
-  .option("identity-pool-id", {
-    describe: "Cognito identity pool id",
-    demandOption: true
-  })
-  .option("invoke-url", {
-    describe: "API Gateway URL",
-    demandOption: true
-  })
-  .option("api-gateway-region", {
-    describe: "API Gateway region",
-    default: "us-east-1"
-  })
-  .option("path-template", {
-    describe: "API path template",
-    demandOption: true
-  })
-  .option("method", {
-    describe: "API method",
-    default: "GET"
-  })
-  .option("params", {
-    describe: "API request params",
-    default: "{}"
-  })
-  .option("additional-params", {
-    describe: "API request additional params",
-    default: "{}"
-  })
-  .option("body", {
-    describe: "API request body",
-    default: "{}"
-  })
-  .help("h")
-  .alias("h", "help")
-  .alias("v", "version")
-  .version(packageJson.version)
-  .wrap(null).argv;
 
 function authenticate(callback) {
   var poolData = {
@@ -98,13 +41,21 @@ function authenticate(callback) {
 
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function(result) {
+      console.log("Success!");
       callback(result.getIdToken().getJwtToken());
     },
     onFailure: function(err) {
       console.log(err.message ? err.message : err);
     },
-    newPasswordRequired: function() {
-      console.log("Given user needs to set a new password");
+    newPasswordRequired: function(userAttributes, requiredAttributes) {
+      console.log("Changing password");
+      delete userAttributes.email_verified;
+      
+      // unsure about this field, but I don't send this back
+      delete userAttributes.phone_number_verified;
+  
+      // Get these details and call
+      cognitoUser.completeNewPasswordChallenge(argv.newPassword, userAttributes, this);
     },
     mfaRequired: function() {
       console.log("MFA is not currently supported");
